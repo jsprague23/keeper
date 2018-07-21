beef1a43c27605c5a1e186e8ae89649abc7c2d4f
Jeremiah Sprague <jeremiahsprague23@gmail.com>
2018-07-21 17:55:00 -0600

    stubbed startup, appsettings json appsetings dev and testsql next pstmn

--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,14 +11,18 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using UserRepository;
+using MySql.Data.MySqlClient;
 
 namespace keeper
 {
     public class Startup
     {
+        private readonly string _connectionString;
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            _connectionString = configuration.GetSection("DB").GetValue<string>("MySQLConnectionString");
         }
 
         public IConfiguration Configuration { get; }
@@ -25,7 +30,40 @@ namespace keeper
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
+            {
+            options.LoginPath = "/Account/Login";
+            options.Events.OnRedirectToLogin = (context) =>
+            {
+                context.Response.StatusCode = 401;
+                return Task.CompletedTask;
+            };
+            });
+
+            services.AddCors(options => 
+            {
+                options.AddPolicy("CorsDevPolicy", builder =>
+                {
+                    builder
+                    .AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials();
+
+                });
+            });
+        
+
+        services.AddMvc();
+        services.AddTransient<IDbConnection>(x => CreateDbContext());
+        services.AddTransient<UserRepository>();
+        }
+
+        private IDbConnection CreateDbContext()
+        {
+            var connection = new MySQLConnection(_connectionString);
+            connection.Open();
+            return connection;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,12 +72,11 @@ namespace keeper
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseCors("CorsDevPolicy");
             }
-            else
-            {
-                app.UseHsts();
-            }
-
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseAuthentication();
             app.UseHttpsRedirection();
             app.UseMvc();
         }
