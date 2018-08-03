6206762e03bbd2e4d923eb3d2ba12aacc130679f
Jeremiah Sprague <jeremiahsprague23@gmail.com>
2018-08-03 15:29:42 -0600

    keep details written out

--- wwwsrc/src/components/KeepDetails.vue
+++ wwwsrc/src/components/KeepDetails.vue
@@ -1,23 +1,60 @@
 <template>
   <div class="KeepDetails">
     <div class="row">
-      <div class="col">
-
-      </div>
+      <h1>Keep Details for {{activeKeep.name}}</h1>
+      <div class="card-columns">
+        <div class="card col-6">
+          <h4 class="logoFont">Description: {{activeKeep.description}}</h4>
+          <h4 class="logoFont">Views: {{activeKeep.views}}</h4>
+          <h4 class="logoFont">Keeps: {{activeKeep.keepCount}}</h4>
+          <img :src="activeKeep.image">
+          <select @click="editKeep(Keep)" v-model="selected">
+              <option v-for="option in options" v-bind:value="option.value">
+                {{option.text}}
+              </option>
+            </select>
+            <span>Selected: {{selected}}</span>
+          <button v-if="currentUser" class="btn btn-danger" @click="deleteKeep(activeKeep.id)">Delete</button>
+        </div>
+      </div>      
     </div>
   </div>
 </template>
 
 <script>
   export default {
-    name: '',
+    name: 'KeepDetails',
     data() {
+      selected: 'Public'
+      options: [{
+          text: 'Public',
+          value: 'public'
+        },
+        {
+          text: 'Private',
+          value: 'private'
+        }
+      ]
       return {
 
       }
     },
-    computed: {},
-    methods: {}
+    computed: {
+      currentUser(){
+        return this.$store.state.currentUser
+      },
+      activeKeep(){
+        this.$store.state.activeKeep
+      },
+      editKeep(id){
+        this.$store.dispatch('editKeep',id)
+      }
+    },
+    methods: {
+      deleteKeep(id){
+        this.$store.dispatch('deleteKeep', id)
+      }
+    }
   }
 
 </script>
